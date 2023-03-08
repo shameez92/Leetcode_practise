58c4f9d5406ccfaaf009a0d690dccab1da133e2b
shamees <shamees@amadeus.ae>
2023-03-08 08:51:06 +0530

    Update in logic

--- CheckIfPalindrome/palindrome.js
+++ CheckIfPalindrome/palindrome.js
@@ -1,15 +1,17 @@
-const givenNumber = 121;
+const givenNumber = 121211;
 let reversedNumber=0;
 
 function isPalindromeNumber(givenNumber){
+
+    if(givenNumber < 0) return false;
+
     let intermediateNumber = givenNumber;
     while(intermediateNumber >= 1){
         let rem = intermediateNumber % 10;
         reversedNumber = reversedNumber * 10 + rem;
         intermediateNumber = parseInt(intermediateNumber/10);
     }
-    if(givenNumber == reversedNumber) { return true; }
-    else { return false; }
+    return givenNumber === reversedNumber;
 }
 
 console.log(isPalindromeNumber(givenNumber));
